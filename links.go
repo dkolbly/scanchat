6155170b8f1cb62e301c3b7391b89f701d5c7192
Donovan Kolbly <donovan@rscheme.org>
2016-12-17 13:20:00 -0600

    Fix URL RE

--- links.go
+++ links.go
@@ -9,7 +9,7 @@ import (
 // fail "safe" where safe is defined as recognizing URLs).  This turns out to be
 // a dark art, and most likely each organization should have a standard one that we
 // should use here.
-var urlRe = regexp.MustCompile(`#(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s` + "`" + `!()\[\]{};:'".,<>?«»“”‘’]))#iS`)
+var urlRe = regexp.MustCompile(`(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s` + "`" + `!()\[\]{};:'".,<>?«»“”‘’]))`)
 
 // parseLink parses out an URL link, if any, from a chat message
 // starting at the `h`, adding it to the Analysis.  Returns the remainder
